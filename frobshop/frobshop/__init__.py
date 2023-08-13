fed3e2e3a19575be4c070125cd31de1d0d04b626
nivdoron1 <nivdoron1234@gmail.com>
2023-08-13 13:43:42 +0300

    deleting the printing

--- frobshop/frobshop/__init__.py
+++ frobshop/frobshop/__init__.py
@@ -2,6 +2,6 @@
 # Django starts so that shared_task will use this app.
 # __init__.py
 
-from .celery import app as celery_app
+#from .celery import app as celery_app
 
-__all__ = ('celery_app',)
+#__all__ = ('celery_app',)
