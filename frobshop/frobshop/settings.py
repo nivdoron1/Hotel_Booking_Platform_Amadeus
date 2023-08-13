0a9445a4dbb8cc3c6455bbaf6f40b32e70ed7c9f
nivdoron1 <nivdoron1234@gmail.com>
2023-08-13 20:04:05 +0300

    change setting.py

--- frobshop/frobshop/settings.py
+++ frobshop/frobshop/settings.py
@@ -173,15 +173,15 @@ HAYSTACK_CONNECTIONS = {
         'ENGINE': 'haystack.backends.simple_backend.SimpleEngine',
     },
 }
+load_dotenv()
 # EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 # EMAIL_BACKEND = "anymail.backends.google.GoogleBackend"
 EMAIL_HOST = 'smtp.outlook.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'nivdoron1234@hotmail.com'
-OSCAR_FROM_EMAIL = 'nivdoron1234@hotmail.com'
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+OSCAR_FROM_EMAIL = os.getenv('EMAIL_HOST_USER')
 
-load_dotenv()
 
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
 # DEFAULT_FROM_EMAIL = EMAIL_HOST_USER  # Default from email
@@ -237,5 +237,14 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 OSCAR_SHOP_NAME = "HOLIOLI"
 LOGIN_URL = '/accounts/login/'
 
+SECURE_SSL_REDIRECT = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+SECURE_HSTS_SECONDS = 3600  # Consider a higher value in actual production
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+SECURE_HSTS_PRELOAD = True
+SECURE_CONTENT_TYPE_NOSNIFF = True
+SECURE_BROWSER_XSS_FILTER = True
+X_FRAME_OPTIONS = 'DENY'
 
 OSCAR_PRODUCTS_PER_PAGE = 150
